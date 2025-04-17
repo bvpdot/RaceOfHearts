df69f56788008223d86486bdb2ba05629935bde6
bvpdot <00001098150879sp@al.educacao.sp.gov.br>
2025-04-17 18:59:16 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+let xJogador = [0, 0, 0, 0, 0, 0];
+let yJogador = [75, 140, 205, 275, 350];
+let jogador = ["❤️", "💟", "💗", "💘", "💕"];
+let teclas = ["a", "s", "d", "f", "g"];
+let quantidade = jogador.length;
+
+function draw() {
+  ativaJogo();
+  desenhaJogadores();
+  desenhaLinhaDeChegada();
+  verificaVencedor();
+}
+
+function ativaJogo() {
+  if (focused == true) {
+    background("#FD858E");
+  } else {
+    background("rgb(253,177,212)");
+  }
+}
+
+function desenhaJogadores() {
+  textSize(40);
+  for (let i = 0; i < quantidade; i++) {
+    text(jogador[i], xJogador[i], yJogador[i]);
+  }
+}
+
+function desenhaLinhaDeChegada() {
+  fill("white");
+  rect(350, 0, 10, 400);
+  fill("black");
+  for (let yAtual = 0; yAtual < 400; yAtual += 20) {
+    rect(350, yAtual, 10, 10);
+  }
+}
+
+function verificaVencedor() {
+  for (let i = 0; i < quantidade; i++) {
+    if (xJogador[i] > 350) {
+      text("heart:" + jogador[i] + "win!", 70, 200);
+      noLoop();
+    }
+  }
+}
+
+function keyReleased() {
+  for (let i = 0; i < quantidade; i++) {
+    if (key == teclas[i]) {
+      xJogador[i] += random(20);
+    }
+  }
+}
